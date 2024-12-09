0d5b616744d0ccdb3e47a511de0f06fc07c8992a
Edward Kemp <edwardlanekemp@gmail.com>
2024-12-09 15:11:02 +0000

    refactor(context-menu): update task selection logic

--- frontend/src/components/context-menu/index.tsx
+++ frontend/src/components/context-menu/index.tsx
@@ -2,17 +2,19 @@ import { FC } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@redux/store";
 import { deleteActiveTasks, toggleSelectedTasks } from "@redux/slices/taskSlice";
+import { Task } from "src/types/task";
 
 const GlobalContextMenu: FC = () => {
   const dispatch = useDispatch();
-  const activeTaskCount = useSelector((state: RootState) => state.tasks.activeTasks.length);
-  const activeTasks = useSelector((state: RootState) => state.tasks.activeTasks);
+  const tasks = useSelector((state: RootState) => state.tasks.tasks);
+  const numOfSelected = tasks.filter((task: Task) => task.selected).length;
+  const selectedTasks = tasks.filter((task: Task) => task.selected).map((task: Task) => task._id);
 
-  if (activeTaskCount === 0) return null;
+  if (numOfSelected === 0) return null;
 
   return (
     <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-gray-800 shadow-lg rounded-md border p-4 flex gap-4 justify-center items-center">
-      <span>{activeTaskCount} task(s) selected</span>
+      <span>{numOfSelected} task(s) selected</span>
 
       <button
         onClick={() => dispatch(toggleSelectedTasks())}
@@ -21,7 +23,7 @@ const GlobalContextMenu: FC = () => {
         Toggle Selected
       </button>
       <button
-        onClick={() => dispatch(deleteActiveTasks(activeTasks))}
+        onClick={() => dispatch(deleteActiveTasks(selectedTasks))}
         className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
       >
         Delete Selected
